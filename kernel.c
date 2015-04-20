1806530e7e4cabdf59d5723f83392da862f04cb2
RainbowZephyr <darkfeathers9@gmail.com>
2015-04-20 18:59:21 +0200

    fixing M4

--- kernel.c
+++ kernel.c
@@ -7,25 +7,41 @@ int mod(int, int);
 void readFile(char[], char[]);
 void executeProgram(char* , int);
 void terminate();
-void writeSector(char[], char[]);
+void writeSector(char*, int);
 void deleteFile(char*);
 void writeFile(char* , char*, int);
 
 
 int main(){
 
-makeInterrupt21();
-interrupt(0x21,4, "shell", 0x2000, 0); //write file testW
+  makeInterrupt21();
+  interrupt(0x21,4, "shell\0", 0x2000, 0); //write file testW//
+  // interrupt(0x21, 4, "tstpr2\0", 0x2000, 0);
+  // interrupt(0x21, 7,"messag\0", 0, 0);
+  // interrupt(0x21, 3, "messag\0", buffer, 0); // try to read messag
+  // interrupt(0x21, 0, buffer, 0, 0);
 
-while(1);
+  //   int i=0;
+  //   char buffer1[13312];
+  //   char buffer2[13312];
+  //   buffer2[0]='h'; buffer2[1]='e'; buffer2[2]='l'; buffer2[3]='l';
+  //   buffer2[4]='o';
+  //   for(i=5; i<13312; i++) buffer2[i]=0x0;
+  //   makeInterrupt21();
+  //   interrupt(0x21,8, "testW\0", buffer2, 1); //write file testW
+  //   interrupt(0x21,3, "testW\0", buffer1, 0); //read file testW
+  //   interrupt(0x21,0, buffer1, 0, 0); // print out contents of testW
+
+
+  while(1);
 
 }
 
 void printString(char* x){
   int i =0 ;
-   char letter;
+  char letter;
   while(x[i] != '\0'){
-   letter = x[i];
+    letter = x[i];
     interrupt(0x10, 0xE*256+letter, 0, 0, 0);
     i++;
   }
@@ -39,15 +55,31 @@ void readString(char string[]){
     switch(x){ //enter
       case 0xD: string[j] = 0xD; string[++j] = 0xA; string[++j] = 0x0; interrupt(0x10, 0xE*256+' ', 0, 0, 0); interrupt(0x10, 0xE*256+x, 0, 0, 0); interrupt(0x10, 0xE*256+0xA, 0, 0, 0); return;
       break; //delete
-      case 0x8: if(j>0){
-         interrupt(0x10, 0xE*256+0x8, 0, 0, 0);
-	  interrupt(0x10, 0xE*256+0x0, 0, 0, 0);
-	  interrupt(0x10, 0xE*256+0x8, 0, 0, 0);
-	  j--; 
-	  } else {
-	  string[j] = x;
-	  interrupt(0x10, 0xE*256+string[j], 0, 0, 0);
-	  }break;
+      case 0x8: if(j>0){//     i=0;
+	//     while(i<7){
+	//       if(line[i] != exec[i]){
+	// 	ce = 0;
+	// 	break;
+	//       }
+	//       i++;
+	//     }
+
+	//     i=0;
+	//     while(i<6){
+	//       if(line[i] != delete[i]){
+	// 	cd = 0;
+	// 	break;
+	//       }
+	//       i++;
+	//     }
+	interrupt(0x10, 0xE*256+0x8, 0, 0, 0);
+	interrupt(0x10, 0xE*256+0x0, 0, 0, 0);
+	interrupt(0x10, 0xE*256+0x8, 0, 0, 0);
+	j--;
+      } else {
+	string[j] = x;
+	interrupt(0x10, 0xE*256+string[j], 0, 0, 0);
+      }break;
       default: string[j] = x; interrupt(0x10, 0xE*256+x, 0, 0, 0); j++;
       break;
     }
@@ -81,19 +113,19 @@ void readSector(char* buffer, int sector){
 }
 
 
- void handleInterrupt21(int ax, int bx, int cx, int dx){
-	switch(ax){
-	case 0: printString(bx); break;
-	case 1: readString(bx); break;
-	case 2: readSector(bx,cx); break;
-	case 3: readFile(bx,cx); break;
-	case 4: executeProgram(bx,cx); break;
-	case 5: terminate(); break;
-	case 6: writeSector(bx,cx); break;
-	case 7: deleteFile(bx); break;
-	case 8: writeFile(bx,cx,dx);  break;
-	default: printString("Unknown Interrupt"); break;
-	}
+void handleInterrupt21(int ax, int bx, int cx, int dx){
+  switch(ax){
+    case 0: printString(bx); break;
+    case 1: readString(bx); break;
+    case 2: readSector(bx,cx); break;
+    case 3: readFile(bx,cx); break;
+    case 4: executeProgram(bx,cx); break;
+    case 5: terminate(); break;
+    case 6: writeSector(bx,cx); break;
+    case 7: deleteFile(bx); break;
+    case 8: writeFile(bx,cx,dx);  break;
+    default: printString("Unknown Interrupt"); break;
+  }
 
 }
 
@@ -109,29 +141,29 @@ void readFile(char name[], char buffer[]){
 
 
   while(i < 16 ){
-        if(checkName(name, directorySector, i) == 0){
-            i++;
-        } else {
-            break;
-        }
+    if(checkName(name, directorySector, i) == 0){
+      i++;
+    } else {
+      break;
+    }
   }
 
   if(i == -1){
     return;
   }
-	while(c<32){
-	if(directorySector[32*i + c] == 0x0){
-		break;
-	}
-		readSector(tmpBuffer,directorySector[32*i+c]);
+  while(c<32){
+    if(directorySector[32*i + c] == 0x0){
+      break;
+    }
+    readSector(tmpBuffer,directorySector[32*i+c]);
 
-		while(tmp < 512){
-			buffer[bufferCounter]=tmpBuffer[tmp];
-			tmp++;
-			bufferCounter++;
-		}
-		c++;
-	}
+    while(tmp < 512){
+      buffer[bufferCounter]=tmpBuffer[tmp];
+      tmp++;
+      bufferCounter++;
+    }
+    c++;
+  }
 
 }
 
@@ -148,21 +180,21 @@ int checkName(char name[] , char directoryName[],  int i){
 
 
 void executeProgram(char* name, int segment){
-	int i;
-	char buffer[13312];
+  int i = 0;
+  char buffer[13312];
 
-	readFile(name, buffer);
+  readFile(name, buffer);
 
-	while(i < 13312){
-		putInMemory(segment,0x0000+i ,buffer[i]);
-		i++;
-	}
-	launchProgram(segment);
+  while(i < 13312){
+    putInMemory(segment, i ,buffer[i]);
+    i++;
+  }
+  launchProgram(segment);
 
 }
 void terminate(){
-	//while(1);
-	interrupt(0x21, 4, "shell\0", 0x2000, 0);
+  //while(1);
+  interrupt(0x21, 4, "shell\0", 0x2000, 0);
 
 }
 
@@ -182,29 +214,29 @@ void deleteFile(char* name){
   int c=6;
   int tmp=0;
   int bufferCounter =0 ;
-  
+
   readSector(directorySector, 2);
   readSector(map, 1);
 
   while(i < 16 ){
-        if(checkName(name, directorySector, i) == 0){
-            i++;
-        } else {
-            break;
-        }
+    if(checkName(name, directorySector, i) == 0){
+      i++;
+    } else {
+      break;
+    }
   }
 
-	directorySector[32*i] = 0x0;
-	writeSector(directorySector,2);
+  directorySector[32*i] = 0x0;
+  writeSector(directorySector,2);
 
   if(i == -1){
     return;
   }
-	while(c<32){
-		map[directorySector[32*i + c]+1] = 0x0;
-		c++;
-	}
-		writeSector(map , 1);
+  while(c<32){
+    map[directorySector[32*i + c]+1] = 0x0;
+    c++;
+  }
+  writeSector(map , 1);
 }
 
 void writeFile(char* name, char* buffer, int secNum){
@@ -222,78 +254,78 @@ void writeFile(char* name, char* buffer, int secNum){
   int tmp;
   int globalCounter = 0;
   int bufferWrites;
-  
-  
+
+
   readSector(directorySector, 2); //load directory
   readSector(map, 1); //load map
-  
-  	while(buffer[bufferSize] != '\0'){  //check file size
-  		bufferSize++;
-  	}
-  	
-  	bufferWrites = divide(bufferSize ,512);
-  	
-  	if(mod(bufferSize, 512) != 0){
-  		bufferWrites++;
-  	}
 
-	while(dirCounter < 16){ // check for empty directory
-		if(directorySector[dirCounter*32] == 0x0){
-			freeDir =1;
-			break;		
-		} else{
-			dirCounter++;
-		}
-	}
-	
-	if(freeDir == 0){
-		return;
-	}
-	
-	while(name[fileCounter] != '\0'){ // copy name of file
-		directorySector[dirCounter*32 + fileCounter] = name[fileCounter];
-		fileCounter++;
-	}
-	
-	if(fileCounter != 5){ //fill rest of name with 0s
-		while(fileCounter < 6){
-			directorySector[dirCounter*32 + fileCounter] = 0x0;
-			fileCounter++;
-		}
-	}
-	
-	while(globalCounter < bufferWrites){
-	
-	mapCounter=0;
-	while(mapCounter < 512){ //find free sector in map
-		if(map[mapCounter] == 0x0){
-			map[mapCounter] = 0xFF;
-			freeSector =1;
-			break;
-		}else{
-			mapCounter++;
-		}
-	}
-	
-	if(freeSector ==0){
-		return;
-	}
-	
-	tmp=0;
-	while(tmp < 512){ //write 512 byte in tmp array
-		tmpBuffer[tmp] = buffer[bufferCounter];
-		tmp++;
-		bufferCounter++; //counter to hold position of data from buffer
-	}
-	
-	directorySector[32 * dirCounter + sectorCounter]  = mapCounter +1;	
-	writeSector(tmpBuffer , mapCounter + 1);
-	writeSector(map ,1);
-	writeSector(directorySector ,2);
-	
-	globalCounter++;
-	}
-	
+  while(buffer[bufferSize] != '\0'){  //check file size
+    bufferSize++;
+  }
+
+  bufferWrites = divide(bufferSize ,512);
+
+  if(mod(bufferSize, 512) != 0){
+    bufferWrites++;
+  }
+
+  while(dirCounter < 16){ // check for empty directory
+    if(directorySector[dirCounter*32] == 0x0){
+      freeDir =1;
+      break;
+    } else{
+      dirCounter++;
+    }
+  }
+
+  if(freeDir == 0){
+    return;
+  }
+
+  while(name[fileCounter] != '\0'){ // copy name of file
+    directorySector[dirCounter*32 + fileCounter] = name[fileCounter];
+    fileCounter++;
+  }
+
+  if(fileCounter != 5){ //fill rest of name with 0s
+    while(fileCounter < 6){
+      directorySector[dirCounter*32 + fileCounter] = 0x0;
+      fileCounter++;
+    }
+  }
+
+  while(globalCounter < bufferWrites){
+
+    mapCounter=0;
+    while(mapCounter < 512){ //find free sector in map
+      if(map[mapCounter] == 0x0){
+	map[mapCounter] = 0xFF;
+	freeSector =1;
+	break;
+      }else{
+	mapCounter++;
+      }
+    }
+
+    if(freeSector ==0){
+      return;
+    }
+
+    tmp=0;
+    while(tmp < 512){ //write 512 byte in tmp array
+      tmpBuffer[tmp] = buffer[bufferCounter];
+      tmp++;
+      bufferCounter++; //counter to hold position of data from buffer
+    }
+
+    directorySector[32 * dirCounter + sectorCounter]  = mapCounter +1;
+    writeSector(tmpBuffer , mapCounter + 1);
+    writeSector(map ,1);
+    writeSector(directorySector ,2);
+
+    globalCounter++;
+  }
+
 }
 
 
--- shell.c
+++ shell.c
@@ -1,121 +1,151 @@
 
 int main (){
-  char line[100];
-  char buffer[13312];
-  int cv,cd,cc,ce;
-  int i,tmp;
-  char* view= "view";
-  char* exec="execute";
-  char* delete="delete";
-  char* copy = "copy";
-  int clear;
-  char* programName;
-  char* fileName;
-
   while(1){
-  cv=1;
-  ce=1;
-  cd=1;
+    char line[100];
+    char buffer[13312];
+    char programName[6];
+    char fileName[6];
     interrupt(0x21, 0, "shell> \0 ", 0, 0);
     interrupt(0x21, 1, line, 0, 0);
-  //  interrupt(0x10, 0xE*256+line[0], 0, 0, 0);
 
-    i=0;
-    while(i < 4){
-      if(line[i] != view[i]){
-	cv = 0;
-	break;
-      }
-      i++;
-    }
-	
-    i=0;
-    while(i<7){
-      if(line[i] != exec[i]){
-	ce = 0;
-	break;
-      }
-      i++;
-    }
-    
-    i=0;
-    while(i<6){
-      if(line[i] != delete[i]){
-	cd = 0;
-	break;
-      }
-      i++;
-    }
-    
-     i=0;
-    while(i<4){
-      if(line[i] != delete[i]){
-	cc = 0;
-	break;
-      }
-      i++;
-    }
-    
+/*    if(line[0] == 'v' && line[1] == 'i' && line[2] == 'e' && line[3] == 'w' ){
+      programName[0] = line[5];
+      programName[1] = line[6];
+      programName[2] = line[7];
+      programName[3] = line[8];
+      programName[4] = line[9];
+      programName[5] = line[10];
 
-    if((cv ==0 && ce == 0 && cd ==0 && cc == 0) ){
-      interrupt(0x21, 0, "bad command", 0, 0);
-    }
-
-
-/*
-      if(cv==1){
-      i=5;
-      while(line[i] != '\0'){
-      programName[i-5] = line[i];
-      i++;
-      }
       interrupt(0x21, 3,programName, buffer, 0);
       interrupt(0x21, 0, buffer, 0, 0);
-      } 
+    } else {
+      interrupt(0x21, 0, "Bad Command\0 ", 0, 0);
+    } */
+
+/*    if(line[0] == 'e' && line[1] == 'x' && line[2] == 'e' && line[3] == 'c' && line[4] == 'u' && line[5] == 't' && line[6] == 'e' ){
+
+      programName[0] = line[8];
+      programName[1] = line[9];
+      programName[2] = line[10];
+      programName[3] = line[11];
+      programName[4] = line[12];
+      programName[5] = line[13];
+
+    } else {
+      interrupt(0x21, 0, "Bad Command\0 ", 0, 0);
+    } */
+
+/*    if(line[0] == 'c' && line[1] == 'o' && line[2] == 'p' && line[3] == 'y'){
+
+      programName[0] = line[5];
+      programName[1] = line[6];
+      programName[2] = line[7];
+      programName[3] = line[8];
+      programName[4] = line[9];
+      programName[5] = line[10];
+
+      fileName[0] =  line[12];
+      fileName[1] =  line[13];
+      fileName[2] =  line[14];
+      fileName[3] =  line[15];
+      fileName[4] =  line[16];
+      fileName[5] =  line[17];
+
+      interrupt(0x21, 3,programName, buffer, 0);
+      interrupt(0x21, 8, fileName, buffer , 0);
+      interrupt(0x21, 0,buffer, 0, 0);
+    } else {
+      interrupt(0x21, 0, "Bad Command\0 ", 0, 0);
+    }
 */
-//cannot have both if conditions at the same time, one must be commented for the other to work
-  /*    if(ce==1){
-	i=8;
-	while(line[i] != '\0'){
-	  programName[i-8] = line[i];
-	  i++;
+ /*   if(line[0] == 'd' && line[1] == 'e' && line[2] == 'l' && line[3] == 'e' && line[4] == 't' && line[5] == 'e'){
+
+      programName[0] = line[6];
+      programName[1] = line[7];
+      programName[2] = line[8];
+      programName[3] = line[9];
+      programName[4] = line[10];
+      programName[5] = line[11];
+
+      interrupt(0x21, 7, programName, 0, 0);
+    } else {
+      interrupt(0x21, 0, "Bad Command\0 ", 0, 0);
+    }
+
+*/
+    if(line[0] == 'd' && line[1] == 'i' && line[2] == 'r'){
+
+      char directory[512];
+      int i = 0;
+      int c = 6;
+      int used = 1;
+
+      char name[6];
+      char size[5];
+      interrupt(0x21, 2, directory, 2, 0);
+
+      while(i<16){
+	if(directory[i*32] != 0x0){
+
+	  name[0] = directory[i*32+0];  //0
+	  name[1] = directory[i*32+1];  //1
+	  name[2] = directory[i*32+2];  //2
+	  name[3] = directory[i*32+3];  //3
+	  name[4] = directory[i*32+4];  //4
+	  name[5] = directory[i*32+5]; //5
+/*
+	  while(directory[i*32 +c] != 0x0){
+	    used++;
+	    c++;
+	  }*/
+
+// 	 switch(used){
+// 	   case 1: size[0] = '5'; size[1] = '1'; size[2] = '2'; break;
+// 	   case 2: size[0] = '1'; size[1] = '0'; size[2] = '2'; size[3] = '4'; break;
+// 	   case 3: size[0] = '1'; size[1] = '5'; size[2] = '3'; size[3] = '6'; break;
+// 	   case 4: size[0] = '2'; size[1] = '0'; size[2] = '4'; size[3] = '8'; break;
+// 	   case 5: size[0] = '2'; size[1] = '5'; size[2] = '6'; size[3] = '0'; break;
+// 	   case 6: size[0] = '3'; size[1] = '0'; size[2] = '7'; size[3] = '2'; break;
+// 	   case 7: size[0] = '3'; size[1] = '5'; size[2] = '8'; size[3] = '4'; break;
+// 	   case 8: size[0] = '4'; size[1] = '0'; size[2] = '9'; size[3] = '6'; break;
+// 	   case 9: size[0] = '4'; size[1] = '6'; size[2] = '0'; size[3] = '8'; break;
+// 	   case 10: size[0] = '5'; size[1] = '1'; size[2] = '2'; size[3] = '0'; break;
+// 	   case 11: size[0] = '5'; size[1] = '6'; size[2] = '3'; size[3] = '2'; break;
+// 	   case 12: size[0] = '6'; size[1] = '1'; size[2] = '4'; size[3] = '4'; break;
+// 	   case 13: size[0] = '6'; size[1] = '6'; size[2] = '5'; size[3] = '6'; break;
+// 	   case 14: size[0] = '7'; size[1] = '1'; size[2] = '6'; size[3] = '8'; break;
+// 	   case 15: size[0] = '7'; size[1] = '6'; size[2] = '8'; size[3] = '0'; break;
+// 	   case 16: size[0] = '8'; size[1] = '1'; size[2] = '9'; size[3] = '2'; break;
+// 	   case 17: size[0] = '8'; size[1] = '7'; size[2] = '0'; size[3] = '4'; break;
+// 	   case 18: size[0] = '9'; size[1] = '2'; size[2] = '1'; size[3] = '6'; break;
+// 	   case 19: size[0] = '9'; size[1] = '7'; size[2] = '2'; size[3] = '8'; break;
+// 	   case 20: size[0] = '1'; size[1] = '0'; size[2] = '2'; size[3] = '4'; size[4] = '0'; break;
+// 	   case 21: size[0] = '1'; size[1] = '0'; size[2] = '7'; size[3] = '5'; size[4] = '2'; break;
+// 	   case 22: size[0] = '1'; size[1] = '1'; size[2] = '2'; size[3] = '6'; size[4] = '4'; break;
+// 	   case 23: size[0] = '1'; size[1] = '1'; size[2] = '7'; size[3] = '7'; size[4] = '6'; break;
+// 	   case 24: size[0] = '1'; size[1] = '2'; size[2] = '2'; size[3] = '8'; size[4] = '8'; break;
+// 	   case 25: size[0] = '1'; size[1] = '2'; size[2] = '8'; size[3] = '0'; size[4] = '0'; break;
+// 	   case 26: size[0] = '1'; size[1] = '3'; size[2] = '3'; size[3] = '1'; size[4] = '2'; break;
+// 	   default: size[0] = 's'; size[1] = 'i'; size[2] = 'z'; size[3] = 'e'; size[4] = '!'; break;
+//
+//
+// 	 }
+
+	  interrupt(0x21, 0, name, 0, 0);
+	//  interrupt(0x21, 0, size, 0, 0);
+	  interrupt(0x21, 0, "\n", 0, 0);
 	}
-	interrupt(0x21, 4, programName, 0x2000, 0);
 
-      } */
- /*     
-	if(cd == 1 ){
-      	i=7;
-      	while(line[i] != '\0'){
-      		programName[i-7]=line[i];
-      		i++;
-      	} 
-      	interrupt(0x21,7 , programName, 0 ,0);
-        } */
+	   i++;
 
-	if(cc == 1 ){
-      	i=4;
-      	while(line[i] != 0x20){
-      		programName[i-4]=line[i];
-      		i++;
-      	} 
-      	i++;
-      	tmp=0;
-      	while(line[i] != '\0'){
-      		fileName[tmp] = line[i];
-      		i++;
-      	}
-      	
-      	interrupt(0x21, 3,programName, buffer, 0);
-      	interrupt(0x21, 8, fileName, buffer , 0);
+
+
       }
 
-
-    clear = 0;
-    while(clear < 100){
-      line[clear]=0x0;
-      clear++;
+    } else {
+      interrupt(0x21, 0, "Bad Command\0 ", 0, 0);
     }
+
 
   }
 }
